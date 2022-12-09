ad334dfea4ed947640e11f3cbf95f67d07e4d2b1
Wesley teo <ling.r.teo@kcl.ac.uk>
2022-12-09 14:32:54 +0000

    Complete things-2

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,13 @@
 """Forms of the project."""
+import django.forms as forms
+from .models import Thing
 
 # Create your forms here.
+class Form(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {
+            'description': forms.Textarea(),
+            'quantity': forms.NumberInput(),
+        }
--- things/views.py
+++ things/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .forms import Form
 
 def home(request):
+    form = Form()
     return render(request, 'home.html', {'form': form})
